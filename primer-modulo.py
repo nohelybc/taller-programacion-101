03927b321fd37e71aaea33d1d8799dc471a0837d
Nohely Badillo Castillo <nohelybadillocastillo@MacBook-Air-de-Nohely.local>
2024-01-13 09:46:36 -0600

    Ejercicios de primer mpodulo

---
+++ primer-modulo.py
@@ -0,0 +1,25 @@
+
+print("Hello World")
+
+# tipos de variables en python
+nombre = "Juan"
+edad = 25
+estatura = 1.75
+tiene_mascota = True
+
+# uniendo un string con variables
+saludo = "Hola, soy " + nombre + " tengo " + str(edad) + " años y mido " + str(estatura) + " metros"
+
+print(saludo)
+
+# operaciones aritméticas
+suma = 5 + 5
+resta = 5 - 5
+multiplicacion = 5 * 5
+division = 5 / 5
+modulo = 5 % 5 # residuo de la division, e.g 5 % 2 = 1
+exponente = 5 ** 5
+
+# input de usuario
+nombre = input("Ingresa tu nombre: ")
+print("Hola", nombre)

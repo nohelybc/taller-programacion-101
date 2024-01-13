30305b287b0f03c37821af23bb2f940d2fc1d6fc
Nohely Badillo Castillo <nohelybadillocastillo@MacBook-Air-de-Nohely.local>
2024-01-13 09:56:20 -0600

    Ejercicios de condicionales

---
+++ segundo-modulo.py
@@ -0,0 +1,18 @@
+# if conditional
+if 5 > 3:
+    print("5 es mayor que 3")
+
+# if else conditional
+if 5 < 3:
+    print("5 es menor que 3")
+else:
+    print("5 no es menor que 3")
+
+# ejercicio 1
+numero = int(input("Ingresa un número: "))
+if numero > 5:
+    print("El número es mayor que 5")
+elif numero == 5:
+    print("El número es igual a 5")
+else:
+    print("El número es menor que 5")

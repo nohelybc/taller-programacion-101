21adc3ae03acf0450d5eb8fbd25336601ee6b151
Nohely Badillo Castillo <nohelybadillocastillo@MacBook-Air-de-Nohely.local>
2024-01-14 22:56:05 -0600

    Piedra papel tijera básico

---
+++ piedra-papel-tijera.py
@@ -0,0 +1,26 @@
+# Build a basic rock-paper-scissors game
+
+import random
+
+def game():
+    user = input("What's your choice? 'r' for rock, 'p' for paper, 's' for scissors\n")
+    user = user.lower()
+
+    computer = random.choice(['r', 'p', 's'])
+
+    if user == computer:
+        return "It's a tie"
+
+    if is_win(user, computer):
+        return "You won!"
+
+    return "You lost!"
+
+def is_win(player, opponent):
+    # return true if player wins
+    # r > s, s > p, p > r
+    if (player == 'r' and opponent == 's') or (player == 's' and opponent == 'p') \
+        or (player == 'p' and opponent == 'r'):
+        return True
+    
+print(game())

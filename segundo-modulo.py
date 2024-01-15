f85be7ac0f3e4fd0ced2ade867527295e95d7a93
Nohely Badillo Castillo <nohelybadillocastillo@MacBook-Air-de-Nohely.local>
2024-01-14 22:49:35 -0600

    Ejercicios con loops y funciones

--- segundo-modulo.py
+++ segundo-modulo.py
@@ -16,3 +16,30 @@ elif numero == 5:
     print("El número es igual a 5")
 else:
     print("El número es menor que 5")
+
+# ejemplo de for loop
+for i in range(5):
+    print(i) # i debe de imprimirse 5 veces, el resultado es 0, 1, 2, 3, 4
+
+# ejemplo de while loop
+i = 0
+while i < 5:
+    print(i) # i debe de imprimirse 5 veces, el resultado es 0, 1, 2, 3, 4
+    i += 1 # i = i + 1
+
+# ejemplo 2 con for loop
+saludo = "Hola mundo"
+for letra in saludo:
+    print(letra) # letra debe de imprimirse 10 veces, el resultado es "Hola mundo"
+
+# funciones en python
+def suma(a, b):
+    return a + b
+
+print(suma(5, 5)) # imprime 10
+
+# ejercicio 3 con funciones
+def saludo(nombre):
+    print("Hola", nombre)
+
+saludo("Juan") # imprime "Hola Juan"
